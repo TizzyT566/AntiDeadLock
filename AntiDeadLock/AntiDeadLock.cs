382b1f6c75acefc22eef396c954630bd08e9cfee
Tien Hyun <tizzyt566@gmail.com>
2021-12-24 14:37:23 -0500

    Add project files.

---
+++ AntiDeadLock/AntiDeadLock.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+
+namespace System.Threading;
+
+/// <summary>
+/// Locks multiple objects in an order that prevents deadlocking.
+/// </summary>
+public class AntiDeadLock
+{
+    private class ObjectHashCode64Comparer : IComparer
+    {
+        public int Compare(object x, object y) => x.GetHashCode64().CompareTo(y.GetHashCode64());
+    }
+
+    private readonly object[] _objects;
+
+    /// <summary>
+    /// Creates a new AntiDeadLock object and locks the objects specified.
+    /// </summary>
+    /// <param name="objects">The list of objects to lock.</param>
+    public AntiDeadLock(params object[] objects)
+    {
+        try { }
+        finally
+        {
+            if (objects != null && objects.Length > 1)
+            {
+                for (int i = 0; i < objects.Length - 1; i++)
+                    for (int j = i + 1; j < objects.Length; j++)
+                        if (objects[i] == objects[j])
+                            throw new ArgumentException("Cannot lock multiple times of the same object.");
+                foreach (object obj in objects)
+                    if (obj == null || obj.GetType().IsValueType)
+                        throw new Exception("Cannot lock null or value types.");
+                Array.Sort(objects, new ObjectHashCode64Comparer());
+                _objects = objects;
+                foreach (object obj in objects)
+                {
+                    if (!Monitor.IsEntered(obj))
+                        Monitor.Enter(obj);
+                }
+            }
+            else
+                throw new ArgumentException("Must provide 2 or more non-null objects for locking.");
+        }
+    }
+
+    /// <summary>
+    /// Release all locked objects.
+    /// </summary>
+    public void ReleaseLocks()
+    {
+        try { }
+        finally
+        {
+            foreach (object obj in _objects)
+                if (Monitor.IsEntered(obj))
+                    Monitor.Exit(obj);
+        }
+    }
+}
+
+
+
+/// <summary>
+/// Exposes GetHashCode64() method.
+/// </summary>
+public static class ObjectExtenstions
+{
+    /// <summary>
+    /// Generates a 64bit hashcode.
+    /// </summary>
+    /// <param name="this">The object to generate the hashcode for.</param>
+    /// <returns>A long representing the 64 bit hashcode.</returns>
+    public static long GetHashCode64(this object @this)
+    {
+        long result = @this.GetType().GetHashCode();
+        result <<= 32;
+        return result + @this.GetHashCode();
+    }
+}
---
+++ AntiDeadLock_Test/Program.cs
@@ -0,0 +1,50 @@
+﻿object obj1 = "Resource A";
+object obj2 = "Resource B";
+
+bool AntiDeadLockTest = true;
+
+if (AntiDeadLockTest)
+{
+    ThreadPool.QueueUserWorkItem(_ =>
+    {
+        Monitor.Enter(obj1);
+        while (true)
+        {
+            AntiDeadLock antiDeadLock = new(obj2, obj1);
+            Console.WriteLine("Thread B completed.");
+            antiDeadLock.ReleaseLocks();
+        }
+    });
+    while (true)
+    {
+        AntiDeadLock antiDeadLock = new(obj1, obj2);
+        Console.WriteLine("Thread A completed.");
+        antiDeadLock.ReleaseLocks();
+    }
+}
+else
+{
+    ThreadPool.QueueUserWorkItem(_ =>
+    {
+        while (true)
+        {
+            lock (obj2)
+            {
+                lock (obj1)
+                {
+                    Console.WriteLine("Thread B completed.");
+                }
+            }
+        }
+    });
+    while (true)
+    {
+        lock (obj1)
+        {
+            lock (obj2)
+            {
+                Console.WriteLine("Thread A completed.");
+            }
+        }
+    }
+}
